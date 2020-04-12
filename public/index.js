21adfcc7e111259770d8fb16f87f0740334d37d2
saeedhassansolangi <saeedhassansolangi@gmail.com>
2020-04-12 06:48:55 +0000

    added all files to branch-2

--- public/index.js
+++ public/index.js
@@ -1,27 +1,27 @@
 var confirmed = document.getElementById("confirmed").innerHTML;
 var recovered = document.getElementById("recovered").innerHTML
 var deaths = document.getElementById("deaths").innerHTML;
+
 console.log(`confirmed : ${confirmed}`);
 console.log(`recovered : ${recovered}`);
 console.log(`deaths : ${deaths}`);
 
 
+
 new Chart(document.getElementById("doughnut-chart"), {
-    type: 'doughnut',
-    data: {
-      labels: ["confirmed","deaths","recovered"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [confirmed,recovered,deaths]
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
+  type: 'doughnut',
+  data: {
+    labels: ["confirmed", "deaths", "recovered"],
+    datasets: [{
+      label: "Population (millions)",
+      backgroundColor: ["#3e95cd", "#de1010", "#32a852"],
+      data: [confirmed, recovered, deaths]
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Predicted world population (millions) in 2050'
     }
+  }
 });
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = 3000
 
 const URL = "https://covid19.mathdro.id/api";
-
+const countriesURL = "https://covid19.mathdro.id/api/countries/";
 // fetch(URL)
 //     .then(response => response.json())
 //     .then(data => console.log(data))
@@ -13,17 +13,59 @@ const URL = "https://covid19.mathdro.id/api";
 
 
 app.use(express.static("public"))
-
+app.use(bodyparser.urlencoded({
+    extended: true
+}))
 
 app.get("/", (req, res) => {
+    res.render("home.ejs")
+})
+
+
+app.get("/total", (req, res) => {
     fetch(URL)
         .then(response => response.json())
         .then(data => {
             // console.log(data)
-            res.render("home.ejs", { data: data })
+            res.render("total.ejs", {
+                data: data
+            })
         })
-    .catch(err => console.log(err))  
+        .catch(err => console.log(err))
 
 })
 
-app.listen(PORT,_=>console.log(`Server is Running on the PORT ${PORT}`))
+
+
+
+app.post("/", (req, res) => {
+    let query = req.body.search;
+    console.log(query);
+
+    const URL = countriesURL + query
+    // console.log(URL);
+    if (query !== "") {
+        fetch(URL)
+            .then(response => response.json())
+            .then(datas => {
+                if (typeof datas.error !== "object") {
+                    console.log(typeof datas.error !== "object");
+
+                    res.render("home.ejs", {
+                        countryInfo: datas
+                    })
+                    // res.render("home.ejs", { data: data })
+                } else {
+                    res.send("This city is not in our database")
+                }
+            })
+            .catch(err => console.log(err))
+    } else {
+        console.log("send send correct info");
+
+        res.send("Send COrrect INfo")
+    }
+
+})
+
+app.listen(PORT, _ => console.log(`Server is Running on the PORT ${PORT}`))
