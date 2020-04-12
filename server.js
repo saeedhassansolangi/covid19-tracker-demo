626a2c49e891fac212184da498b5367c7c71a35a
saeedhassansolangi <saeedhassansolangi@gmail.com>
2020-04-12 08:31:00 +0000

    modify package.json with start script

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyparser = require('body-parser');
 const fetch = require("node-fetch");
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT ||3000
 
 
 const URL = "https://covid19.mathdro.id/api";
